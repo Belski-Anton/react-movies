53832ad91ace3272ce45dbe214efde3837a972a9
Anton Belski <anton.belski@gmail.com>
2024-07-01 19:21:44 +0300

    feat: project complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./loyout/Header";
+import Main from "./loyout/Main";
+import Footer from "./loyout/Footer";
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer/>
+    </>
+  );
+};
+
+export default App;
